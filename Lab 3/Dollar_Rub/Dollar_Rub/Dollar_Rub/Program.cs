fe6bcf27d99f2930a677a6fe548d4c4f8bc663fb
Vladislav <73547205+vladvlad346764@users.noreply.github.com>
2020-11-19 17:09:10 +0300

    upload

---
+++ Lab 3/Dollar_Rub/Dollar_Rub/Dollar_Rub/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dollar_Rub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. цикл for
+
+            /* 
+            double dollaramount;
+            int ir = 5;
+            Console.WriteLine("Введите курс доллара");
+            dollaramount = double.Parse(Console.ReadLine());
+            for (int i = 1; i <= 20; i++) {
+                double emr = ir * dollaramount;
+                Console.WriteLine("{0} = {1}", ir, emr);
+                ir += 5;
+            }
+            */
+
+
+            //2. цикл while
+
+            /*
+            int ir = 5;
+            double dollaramount;
+            Console.WriteLine("Введите курс доллара");
+            dollaramount = double.Parse(Console.ReadLine());
+            int i = 1;
+             while (i <=20) 
+             {
+                double emr = ir * dollaramount;
+                Console.WriteLine("{0} = {1}", ir, emr);
+                ir += 5;
+                i++;
+            } 
+            */
+
+            // 3 . цикл do while
+            /*
+            int ir = 5;
+            double dollaramount;
+            Console.WriteLine("Введите курс доллара");
+            dollaramount = double.Parse(Console.ReadLine());
+            int i = 1;
+            do {
+                double emr = ir * dollaramount;
+                Console.WriteLine("{0} = {1}", ir, emr);
+                ir += 5;
+                i++;
+            } while (i <= 20);
+            */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 3/Output_summ_of_integer/Output_summ_of_integer/Output_summ_of_integer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_summ_of_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // не совсем понял как реализовать самоввод числа программой,
+            //поэтому число вводится вручную пользователем
+
+            Console.WriteLine("Введите целое число");
+            int digital_int = int.Parse(Console.ReadLine());
+            int summafinal = 0;
+            while (digital_int > 0)
+            {
+
+                summafinal += digital_int % 10;
+                digital_int /= 10;
+
+            }
+            Console.WriteLine(summafinal);
+            Console.ReadKey();
+        }
+    }
+}
