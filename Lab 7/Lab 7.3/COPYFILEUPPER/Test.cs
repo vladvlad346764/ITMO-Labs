54b6c1f1b2f1982cc13751dfd667cf01d1b58471
Vladislav <73547205+vladvlad346764@users.noreply.github.com>
2020-11-19 18:27:26 +0300

    lab 7.3 upload

---
+++ Lab 7/Lab 7.3/COPYFILEUPPER/Test.cs
@@ -0,0 +1,51 @@
+﻿USING SYSTEM;
+USING SYSTEM.IO;
+
+/// <SUMMARY>
+///    CLASS TO CREATE AN UPPER CASE COPY OF A FILE
+/// </SUMMARY>
+CLASS COPYFILEUPPER
+{
+    STATIC VOID MAIN()
+
+    {
+        STRING SFROM, STO;
+        STREAMREADER SRFROM;
+        STREAMWRITER SWTO;
+
+        // PROMPT FOR INPUT FILE NAME
+        CONSOLE.WRITE("COPY FROM:");
+        SFROM = CONSOLE.READLINE();
+
+        // PROMPT FOR OUTPUT FILE NAME
+        CONSOLE.WRITE("COPY TO:");
+        STO = CONSOLE.READLINE();
+
+        CONSOLE.WRITELINE("COPY FROM {0} TO {1}", SFROM, STO);
+        TRY
+        {
+            SRFROM = NEW STREAMREADER(SFROM);
+            SWTO = NEW STREAMWRITER(STO);
+
+            WHILE(SRFROM.PEEK() != -1)
+            {
+                STRING SBUFFER = SRFROM.READLINE();
+                SBUFFER = SBUFFER.TOUPPER();
+                SWTO.WRITELINE(SBUFFER);
+            }
+            SWTO.CLOSE();
+            SRFROM.CLOSE();
+
+        }
+        CATCH(FILENOTFOUNDEXCEPTION)
+        {
+            CONSOLE.WRITELINE("INPUT FILE NOT FOUND");
+        }
+        CATCH(EXCEPTION E)
+        {
+            CONSOLE.WRITELINE("UNEXPECTED EXCEPTION");
+            CONSOLE.WRITELINE(E.TOSTRING());
+        }
+        CONSOLE.READKEY();
+    }
+}
