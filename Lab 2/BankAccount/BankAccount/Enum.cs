154be3b61188e28221ec010928f25817ee34fe9e
Vladislav <73547205+vladvlad346764@users.noreply.github.com>
2020-11-19 07:25:07 +0300

    lab 2 upload

---
+++ Lab 2/BankAccount/BankAccount/Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Enum
+    {
+        public enum AccountType { Checking, Deposit }
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+            Console.ReadKey();
+        }
+    }
+}
