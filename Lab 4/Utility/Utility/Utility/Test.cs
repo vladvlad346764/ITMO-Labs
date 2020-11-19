c7c4564d074ef9241507af924bcf1180e9e7f94e
Vladislav <73547205+vladvlad346764@users.noreply.github.com>
2020-11-19 17:23:03 +0300

    lab 4 upload

--- Lab 4/Utility/Utility/Utility/Test.cs
+++ Lab 4/Utility/Utility/Utility/Test.cs
@@ -6,10 +6,93 @@ using System.Threading.Tasks;
 
 namespace Utility
 {
-    class Program
+    public class Utils
     {
-        static void Main(string[] args)
+        public static int Greater(int a, int b)
         {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static bool Factorial(int n, out int answer)
+        {
+            int k;
+            int f;
+            bool ok = true;
+
+            if (n < 0)
+                ok = false;
+
+            try
+            {
+                checked
+                {
+                    f = 1;
+                    for (k = 2; k <= n; ++k)
+                    {
+                        f = f * k;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                f = 0;
+                ok = false;
+            }
+
+            answer = f;
+
+            return ok;
+        }
+    }
+    
+    
+
+    public class Test
+    {
+        static void Main( )
+        {
+            try
+            {
+                int x;
+                int y;
+                int greater;
+                Console.WriteLine("Enter first number:");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number:");
+                y = int.Parse(Console.ReadLine());
+                greater = Utils.Greater(x, y);
+                Console.WriteLine("The greater value is " + greater);
+
+                Console.WriteLine("Before swap: " + x + "," + y);
+                Utils.Swap(ref x, ref y);
+                Console.WriteLine("After swap: " + x + "," + y);
+
+                int f;
+                bool ok;
+                Console.WriteLine("Number for factorial:");
+                x = int.Parse(Console.ReadLine());
+                ok = Utils.Factorial(x, out f);
+                if (ok)
+                    Console.WriteLine("Factorial(" + x + ") = " + f);
+                else
+                    Console.WriteLine("Cannot compute this factorial");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Some error was encountered: {0}", ex.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
