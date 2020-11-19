1fb9c737c5568fd8e5eafcaf3e4306053169ff2b
Vladislav <73547205+vladvlad346764@users.noreply.github.com>
2020-11-19 18:04:53 +0300

    lab 7.1 upload

---
+++ Lab 7/Lab 7.1/BANK/Test.cs
@@ -0,0 +1,27 @@
+﻿namespace Utils
+{
+    using System;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            BankAccount b1 = new BankAccount();
+            b1.Populate(100);
+
+            BankAccount b2 = new BankAccount();
+            b2.Populate(100);
+
+            Console.WriteLine("Before transfer");
+            Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
+            Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+
+            b1.TransferFrom(b2, 10);
+
+            Console.WriteLine("After transfer");
+            Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
+            Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+            Console.ReadKey();
+        }
+    }
+}
