08dd30c98d9975b15631401aaceb6a6b0ebe3b46
Vladislav <73547205+vladvlad346764@users.noreply.github.com>
2020-11-19 18:10:59 +0300

    lab 7.2 upload

---
+++ Lab 7/Lab 7.2/UTILS/Test.cs
@@ -0,0 +1,20 @@
+﻿namespace Utils
+{
+    using System;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            string message;
+
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            Utils.Reverse(ref message);
+
+            Console.WriteLine(message);
+
+        }
+    }
+}
